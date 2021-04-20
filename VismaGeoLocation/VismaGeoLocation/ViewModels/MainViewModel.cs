c0b0a72dc3c048d067fee98823e0e1d9ac503250
mlvillarroya <mlvillarroya@gmail.com>
2021-04-20 19:28:41 +0200

    code cleaning

--- VismaGeoLocation/VismaGeoLocation/ViewModels/MainViewModel.cs
+++ VismaGeoLocation/VismaGeoLocation/ViewModels/MainViewModel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using VismaGeoLocation.Models;
+﻿using VismaGeoLocation.Models;
 using VismaGeoLocation.Services;
 
 namespace VismaGeoLocation.ViewModels
